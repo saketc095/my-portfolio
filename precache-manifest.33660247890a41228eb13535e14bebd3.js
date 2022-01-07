4973a416dd89920b97e3125eba77810f214e3b3f
saketc095 <saketc095@gmail.com>
2022-01-06 20:32:53 -0700

    Updates

---
+++ precache-manifest.33660247890a41228eb13535e14bebd3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "448349e73af19369ab9b991ab1e829fc",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "1ee66518572798553d91",
+    "url": "/my-portfolio/static/css/2.83148a66.chunk.css"
+  },
+  {
+    "revision": "7be2d6ffa44e636a9a7c",
+    "url": "/my-portfolio/static/css/main.39a5e478.chunk.css"
+  },
+  {
+    "revision": "1ee66518572798553d91",
+    "url": "/my-portfolio/static/js/2.b020f50c.chunk.js"
+  },
+  {
+    "revision": "cab22c2f853eb971190951a87268c280",
+    "url": "/my-portfolio/static/js/2.b020f50c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7be2d6ffa44e636a9a7c",
+    "url": "/my-portfolio/static/js/main.3aa62b84.chunk.js"
+  },
+  {
+    "revision": "5d4511f3fcec99c33304",
+    "url": "/my-portfolio/static/js/runtime-main.c58b4907.js"
+  },
+  {
+    "revision": "e776faf6628ed17bcbeac035f93a3e44",
+    "url": "/my-portfolio/static/media/Resume.e776faf6.pdf"
+  },
+  {
+    "revision": "d4ea38dbe02f1cbfe832086119aef70c",
+    "url": "/my-portfolio/static/media/myPic.d4ea38db.jpg"
+  },
+  {
+    "revision": "0eba51ffd9e317755a6eb0eb355866d7",
+    "url": "/my-portfolio/static/media/myPic2.0eba51ff.jpg"
+  }
+]);
